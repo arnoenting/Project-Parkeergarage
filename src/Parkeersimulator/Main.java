a7f0bbcea8515cf13bd87f5a9431db3f29c175a7
Michaelew <43989931+Michaelew@users.noreply.github.com>
2019-01-21 14:34:21 +0100

    Update Main.java

--- src/Parkeersimulator/Main.java
+++ src/Parkeersimulator/Main.java
@@ -2,7 +2,7 @@ package Parkeersimulator;
 
 public class Main {
 	public static void main(String[] args) {
-		SimulatorView view = new SimulatorView(3, 6, 30);
+		SimulatorView view = new SimulatorView(3, 6, 30); // aantal floors, rows, places
 		Simulator model = new Simulator(view);
 		SimulatorController controller = new SimulatorController(model);
 		view.addController(controller);
